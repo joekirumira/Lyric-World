5b4b195a38223239a5e0ba270def1c18d815163b
joekirumira <joekirumira3@gmail.com>
2020-11-11 15:21:00 +0300

    Corrected mongodb password error

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connectDB = require('../config/db');
 const models = require('./models');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
@@ -7,22 +8,8 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://Hoze:joenamakula3@shopmanager-3dcc8.mongodb.net/graphql?retryWrites=true&w=majority';
-if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
-}
-
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+//Connect Database
+connectDB();
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
